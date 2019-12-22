ca392d5d15558dbc9d556eb9745de1214e35b7fa
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2019-12-23 00:10:16 +0100

    classesand objects

---
+++ Compound/Classesandobjects/Car.java
@@ -0,0 +1,16 @@
+
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+    public String getModel(){
+        return model;
+    }
+}
---
+++ Compound/Classesandobjects/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car beetles = new Car();
+
+        porsche.setModel("punchamba");
+        System.out.println("Model is " + porsche.getModel());
+    }
+}
