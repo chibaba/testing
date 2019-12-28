1b5507c6ad4cad7bc6cef19116a3ff6fee454df7
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2019-12-28 15:37:27 +0100

    wip

--- gamesofdice/MethodOverload.java
+++ gamesofdice/MethodOverload.java
@@ -3,4 +3,14 @@ public  class MethodOverload {
         System.out.printf("Square of integer 7 is %d%n", square(7));
         System.out.printf("Square of double 7.5 is %d%n", square(7.5));
     }
+    // square method with int arguement
+    public static int square(int intValue) {
+            System.out.printf("%nCalled square with int arguement: %d%n", intValue);
+            return intValue * intValue;
+    }
+    //square method with double arguement
+    public static double square(double doubleValue) {
+        System.out.printf("%nCalled square with double arguement: %f%n", doubleValue);
+        return doubleValue * doubleValue;
+    }
 }
