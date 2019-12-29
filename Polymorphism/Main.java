de5000b8c364f70d5f9da6014f1c67d40a0fa9b0
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2019-12-30 00:59:54 +0100

    plymorphism contd

--- Polymorphism/Main.java
+++ Polymorphism/Main.java
@@ -14,6 +14,25 @@ class Jaws extends Movie {
     public Jaws() {
         super("Jaws");
     }
+
+public String plot() {
+    return "shark eats lots of people";
+}
+}
+ class IndependenceDay extends Movie {
+    public IndependenceDay() {
+        super("IndependemceDay");
+    }
+    @Override
+    public String plot() {
+        return ("the aliens are here already");
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("MazeRunner");
+    }
 }
 public class Main {
     public static void main(String[] args) {
