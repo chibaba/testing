fa2abd1df74a50d0049565e48f7d999878256a5d
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2019-12-30 04:05:43 +0100

    polymorphism continues

--- Polymorphism/Main.java
+++ Polymorphism/Main.java
@@ -33,9 +33,46 @@ class MazeRunner extends Movie {
     public MazeRunner() {
         super("MazeRunner");
     }
+    @Override
+    public String plot() {
+        return("apparently we are runnly forward");
+    }
+}
+class StarWars extends Movie {
+    public StarWars() {
+        super("StarWars");
+    }
+    @Override
+    public String plot(){
+        return ("our early phanatasis");
+    }
+}
+class Forgetable extends Movie {
+    public Forgetable() {
+        super("Forgetable");
+    }
+    // write overide here
 }
 public class Main {
     public static void main(String[] args) {
 
     }
+    public static Movie randoMovie() {
+    int randomNumber = (int)(Math.random() * 5) + 1;
+    System.out.println("Random number generated was: " + randomNumber);
+    switch (randomNumber) {
+        case 1:
+        return new Jaws();
+        case 2:
+        return  new IndependenceDay();
+        case 3:
+        return new MazeRunner();
+        case 4:
+        return new StarWars();
+        case 5:
+        return new Forgetable();
+
+    }
+     return null;
+}
 }
