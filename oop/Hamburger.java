e14d45c6def0ba951c6a0e162eb89fc56892e4c2
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-01-04 04:03:10 +0100

    oo

--- oop/Hamburger.java
+++ oop/Hamburger.java
@@ -42,4 +42,25 @@ public class Hamburger {
         this.additional1Name4 = name;
         this.additional1Price4 = price;
     }
+    public double itemisHamburger() {
+        double harmburgerPrice = this.price;
+        System.out.println(this.name + " harmburger " + " on a " + this.bedRollType + " roll "
+        + "price is " + "this.price ");
+        if(this.additional1Name != null) {
+            harmburgerPrice += this.additional1Price;
+            System.out.ptintln("Addes " + this.additional1Name + " for an extra " + this.additional1Price);
+        }
+        if(this.additional2Name != null) {
+            harmburgerPrice += this.additional2Price;
+            System.out.ptintln("Addes " + this.additional2Name + " for an extra " + this.additional2Price);
+        }
+        if(this.additional3Name != null) {
+            harmburgerPrice += this.additional3Price;
+            System.out.ptintln("Addes " + this.additional3Name + " for an extra " + this.additional3Price);
+        }
+        if(this.additional4Name != null) {
+            harmburgerPrice += this.additional1Price;
+            System.out.ptintln("Addes " + this.additional4Name + " for an extra " + this.additional4Price);
+        }
+    }
 }
