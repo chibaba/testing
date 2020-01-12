36c1a35ef97afe341ae1274f5f98a4504eb5c618
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-01-12 03:50:25 +0100

    healthy burger app

---
+++ oop/HealthyBurger.java
@@ -0,0 +1,12 @@
+public class HealthyBurger extends Hamburger {
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+
+    public HealthyBurger( String meat, double price) {
+        super("Healthy", meat, price, "Brown rye");
+        this.healthyExtra1Name = healthyExtra1Name;
+    }
+}
