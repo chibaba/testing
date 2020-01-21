ebb58ecbc33fa7954fbe74f3a05758001ba38c6c
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-01-21 17:32:43 +0100

    wip

--- Smallest1.java
+++ Smallest1.java
@@ -6,6 +6,16 @@ public class Smallest1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in)
         int nums = input.nextInt()
+        while (n>0) {
+            System.out.println("Enter next number");
+            int nums = input.nextInt();
+            smallest = nums
+            if(number < smallest) {
+                smallest = number
+            }
+            n--;
+        }
+        System.out.printlm(smallest)
 
     }
 }
